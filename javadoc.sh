4424bf5820f81c7368ec5b536c37b28d7c3d4657
Kirk Rader <apps@rader.us>
2013-06-12 16:29:52 -0700

    add script to generate javadoc

---
+++ javadoc.sh
@@ -0,0 +1,1 @@
+javadoc -overview WyFy/src/overview.html -source 1.6 -d WyFy/doc -private -sourcepath WyFy/src/ us.rader.wyfy us.rader.wyfy.db us.rader.wyfy.model us.rader.wyfy.nfc us.rader.wyfy.provider
