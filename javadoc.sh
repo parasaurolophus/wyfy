a2785a8371b6bf57c6f38a376ecb59921cc7c803
Kirk Rader <apps@rader.us>
2013-06-12 19:32:21 -0700

    generate javadocs as wyfy.pdf instead of HTML

--- javadoc.sh
+++ javadoc.sh
@@ -1,1 +1,1 @@
-javadoc -overview WyFy/src/overview.html -source 1.6 -d WyFy/doc -private -sourcepath WyFy/src/ us.rader.wyfy us.rader.wyfy.db us.rader.wyfy.model us.rader.wyfy.nfc us.rader.wyfy.provider
+javadoc -doclet com.tarsec.javadoc.pdfdoclet.PDFDoclet -docletpath ./pdfdoclet-1.0.2-all.jar -overview WyFy/src/overview.html -source 1.6 -pdf wyfy.pdf -private -sourcepath WyFy/src/ us.rader.wyfy us.rader.wyfy.db us.rader.wyfy.model us.rader.wyfy.nfc us.rader.wyfy.provider
